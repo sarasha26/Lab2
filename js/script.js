2375c0282656cb5e17ee8596684709a820135741
Saranya Shankar <saranya.shankar@yahoo.com>
2024-10-14 13:20:35 -0700

    Added code

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let milesDriven = prompt("How many miles have you driven?");
+let gallons = prompt("How many gallons of fuel does your car hold?");
+let mpg = milesDriven/gallons;
+document.write(`Your car gets ${mpg.toFixed(2)} miles per gallon`);
